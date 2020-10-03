cb986a9e69a69eb0b639cfbf1cb432d17dcd882b
Aldair <aldaprojects@gmail.com>
2020-10-03 13:01:01 -0500

    Added documentation

--- models/user_model.go
+++ models/user_model.go
@@ -1,7 +1,8 @@
 package models
 
+// User represents an account. It's the object for users account.
 type User struct {
-	Id 		 int	`json:"id"`
+	Id       int    `json:"id"`
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
