7901942b775565066984d252f16f5bdfea079639
Aldair <aldaprojects@gmail.com>
2020-10-03 12:57:40 -0500

    Added documentation

--- db/user_db.go
+++ db/user_db.go
@@ -9,31 +9,37 @@ import (
 	"github.com/aldaprojects/basic-restapi/models"
 )
 
-type DB struct {
+type UserDB struct {
 	mux sync.Mutex
 }
 
+// read reads directly the virtual database and return an array of users
+// A successful call returns an array of users an err == nil. Because users.json always has an array.
+// First of all users.json has an empty array without users in it
 func read() ([]models.User, error) {
-	js, err := ioutil.ReadFile("data/users.json")
+	js, err := ioutil.ReadFile("db/users.json")
 	if err != nil {
 		log.Println(err.Error())
 		return nil, err
 	}
 
 	var users []models.User
-	json.Unmarshal(js, &users)
+	err = json.Unmarshal(js, &users)
 
-	return users, nil
+	return users, err
 }
 
+// write writes a user directly in the virtual database and return an error if there is one.
+// A successful call returns err == nil, that means the new user
+// was pushed correctly in the array from users.json
 func write(users []models.User) error {
-	json, err := json.Marshal(users)
+	js, err := json.Marshal(users)
 	if err != nil {
 		log.Println(err.Error())
 		return err
 	}
 
-	err = ioutil.WriteFile("data/users.json", json, 7777)
+	err = ioutil.WriteFile("db/users.json", js, 7777)
 	if err != nil {
 		log.Println(err.Error())
 		return err
@@ -42,7 +48,9 @@ func write(users []models.User) error {
 	return nil
 }
 
-func (db *DB) Count() (int, error) {
+// Count counts how many users are in the array from users.json
+// A successful call returns how many users are in the array and err == nil.
+func (db *UserDB) Count() (int, error) {
 	db.mux.Lock()
 	defer db.mux.Unlock()
 
@@ -53,7 +61,10 @@ func (db *DB) Count() (int, error) {
 	return len(users), err
 }
 
-func (db *DB) Create(user *models.User) error {
+// Create creates a user.
+// A successful call returns err == nil. First read the database and
+// push the new user in the array from arrays.json then write into the database
+func (db *UserDB) Create(user *models.User) error {
 	db.mux.Lock()
 	defer db.mux.Unlock()
 
@@ -67,7 +78,10 @@ func (db *DB) Create(user *models.User) error {
 	return write(users)
 }
 
-func (db *DB) Read(id int) (models.User, bool, error) {
+// Read get a user from the virtual database.
+// A successful call returns true if user was found and User if User.Id != id.
+// If the user was not found it returns false
+func (db *UserDB) Read(id int) (models.User, bool, error) {
 	db.mux.Lock()
 	defer db.mux.Unlock()
 
@@ -85,7 +99,10 @@ func (db *DB) Read(id int) (models.User, bool, error) {
 	return models.User{}, false, nil
 }
 
-func (db *DB) Update(user models.User) (bool, error) {
+// Update reads the virtual database to get a particular user. Then modify its properties
+// and save the user. A successful call return true if the user was modify
+// correctly and err == nil.
+func (db *UserDB) Update(user models.User) (bool, error) {
 	db.mux.Lock()
 	defer db.mux.Unlock()
 
@@ -107,7 +124,10 @@ func (db *DB) Update(user models.User) (bool, error) {
 
 }
 
-func (db *DB) Delete(id int) (bool, error) {
+// Delete reads the virtual database to get a particular user. Then modify the array
+// to delete the user and override the array into users.json.
+// A successful call returns true if the user was found and deleted correctly and err == nil.
+func (db *UserDB) Delete(id int) (bool, error) {
 	db.mux.Lock()
 	defer db.mux.Unlock()
 
@@ -118,8 +138,11 @@ func (db *DB) Delete(id int) (bool, error) {
 
 	for index, user := range users {
 		if user.Id == id {
-			users[index] = users[len(users) - 1]
-			users = users[:len(users) - 1]
+
+			// Replace the user with the last user from the array.
+			// Then create a slice without the last user.
+			users[index] = users[len(users)-1]
+			users = users[:len(users)-1]
 
 			err := write(users)
 
